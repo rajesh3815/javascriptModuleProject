5a06b9b6888288b0fc2b562f6e8fb2fc654c7343
Rajesh <rkraja730@gmail.com>
2023-12-21 23:00:07 +0530

    ui changes

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const button = document.querySelector('.btn')
 const pop_up = document.querySelector('.rules-pop')
 const close_popup = document.querySelector('.cross')
+
+const users = document.querySelectorAll('.user')
+const game = document.querySelector('.game')
+const result = document.querySelector('.results')
+const res_divs = document.querySelectorAll('.res_div')
+
+const data_choice = ["rock", "paper", "scissor"]
+
 button.addEventListener("click", () => {
     pop_up.style.display = "block"
 
@@ -8,4 +16,29 @@ button.addEventListener("click", () => {
 close_popup.addEventListener("click", () => {
     pop_up.style.display = "none"
 })
+// results displaying
+users.forEach((el, index) => {
+    el.addEventListener("click", (e) => {
+        game.style.display = "none"
+        const choiceName = el.dataset.choice
+        choose(choiceName);
+    })
+})
+function choose(choice) {
+    const computer_pick = computer_p()
+    displayResults([choice, computer_pick])
+}
+function computer_p() {
+    const rand_value = Math.floor(Math.random() * 3);
+    return data_choice[rand_value]
+}
+function displayResults(values) {
+    res_divs.forEach((divs, idx) => {
+        setTimeout(() => {
 
+            divs.innerHTML = `<div class="user ${values[idx]}-res"> <img src="image/${values[idx]}.png" alt="${values[idx]}"></div>`
+            
+        }, 1000 * idx)
+    })
+    result.classList.toggle('hide')
+}
